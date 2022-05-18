b21e3949d61853fbc618c00cb0da08551b47fd0a
GilltonChung <73431104+GilltonChung@users.noreply.github.com>
2022-05-18 16:30:48 +0800

    Add files via upload

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,7 @@
 
 
     //Create Constants to Store Non Repeating Values
-    define('SITEURL', 'http://localhost/');
+    define('SITEURL', 'https://gilltonassignment2.azurewebsites.net/');
     //define('LOCALHOST', 'localhost');
     //define('DB_USERNAME', 'root');
     //define('DB_PASSWORD', '');
